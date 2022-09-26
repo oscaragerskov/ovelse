618bad7fb415d1e97aa20b4853cba8f81cdd6a72
S224551 <112851693+S224551@users.noreply.github.com>
2022-09-26 15:03:58 +0200

    Update Distanceformlen.java

--- src/Distanceformlen.java
+++ src/Distanceformlen.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Distanceformlen {
     public static void main(String[] args) {
-        Scanner input= new Scanner(System.in);
+
 
         System.out.println("Indsæt dine værdier i formlen: dist(P,l)=((a*x1+b-y1*c))/(kvadratrod a^2+b)");
 
